b87b66a3ec67b0bb7baa382900ff54f02d45728d
Michael Schoder <m.schoder@my-realm.net>
2019-10-16 21:24:53 +0200

    feat: created project

---
+++ WristSteps WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WristSteps WatchKit Extension
+//
+//  Created by Michael Schoder on 16.10.19.
+//  Copyright © 2019 Michael Schoder. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ WristSteps WatchKit Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  WristSteps WatchKit Extension
+//
+//  Created by Michael Schoder on 16.10.19.
+//  Copyright © 2019 Michael Schoder. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
