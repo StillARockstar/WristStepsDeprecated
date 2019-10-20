34b2d985e0ca296e5dbb11c01b575f9c42a50ef7
Michael Schoder <m.schoder@my-realm.net>
2019-10-20 21:02:54 +0200

    feat: Added Graphic Corner Template

--- WristSteps WatchKit Extension/Complication/ComplicationController.swift
+++ WristSteps WatchKit Extension/Complication/ComplicationController.swift
@@ -14,43 +14,41 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     // MARK: - Timeline Configuration
     
     func getSupportedTimeTravelDirections(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimeTravelDirections) -> Void) {
-        handler([.forward, .backward])
-    }
-    
-    func getTimelineStartDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
-        handler(nil)
-    }
-    
-    func getTimelineEndDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
-        handler(nil)
+        handler([])
     }
     
     func getPrivacyBehavior(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationPrivacyBehavior) -> Void) {
-        handler(.showOnLockScreen)
+        handler(.hideOnLockScreen)
     }
     
     // MARK: - Timeline Population
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
-        // Call the handler with the current timeline entry
-        handler(nil)
-    }
-    
-    func getTimelineEntries(for complication: CLKComplication, before date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
-        // Call the handler with the timeline entries prior to the given date
-        handler(nil)
-    }
-    
-    func getTimelineEntries(for complication: CLKComplication, after date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
-        // Call the handler with the timeline entries after to the given date
-        handler(nil)
+        switch complication.family {
+        case .graphicCorner:
+            let complication = CLKComplicationTemplateGraphicCornerTextImage()
+            complication.imageProvider = CLKFullColorImageProvider(fullColorImage: UIImage(named:"radialGraph71")!)
+            complication.textProvider = CLKTextProvider(format: "%d steps", 1000)
+
+            let timelineEntry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: complication)
+            handler(timelineEntry)
+        default:
+            handler(nil)
+        }
     }
     
     // MARK: - Placeholder Templates
     
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
-        // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
+        switch complication.family {
+        case .graphicCorner:
+            let complication = CLKComplicationTemplateGraphicCornerTextImage()
+            complication.imageProvider = CLKFullColorImageProvider(fullColorImage: UIImage(named:"radialGraph71")!)
+            complication.textProvider = CLKTextProvider(format: "%d steps", 1000)
+            handler(complication)
+        default:
+            handler(nil)
+        }
     }
     
 }
